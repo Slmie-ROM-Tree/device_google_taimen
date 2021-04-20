43a9eedb04ad87a5e406cb299f7169c96852ac4b
Slmie <2258248722@qq.com>
2021-04-20 08:52:50 +0800

    taimen: Merge aosp_taimen.mk to exthm_taimen.mk

--- exthm_taimen.mk
+++ exthm_taimen.mk
@@ -1,20 +1,56 @@
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2880
-TARGET_SCREEN_WIDTH := 1440
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common exTHmUI stuff.
 $(call inherit-product, vendor/exthm/config/common_full_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/google/taimen/aosp_taimen.mk)
+# Inherit from taimen device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+$(call inherit-product-if-exists, vendor/google_devices/taimen/proprietary/device-vendor.mk)
 
--include device/google/taimen/device-lineage.mk
+# Inherit from taimen vendor
+$(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_NAME := exthm_taimen
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2880
+TARGET_SCREEN_WIDTH := 1440
+
+PRODUCT_MANUFACTURER := Google
 PRODUCT_BRAND := google
+PRODUCT_NAME := exthm_taimen
+PRODUCT_DEVICE := taimen
 PRODUCT_MODEL := Pixel 2 XL
 PRODUCT_RESTRICT_VENDOR_FILES := false
+PRODUCT_RESTRICT_VENDOR_FILES := owner
+
+PRODUCT_PACKAGES += \
+    Dialer \
+    Launcher3QuickStep \
+    WallpaperPicker \
+    netutils-wrapper-1.0 \
+    vndk_package
+
+PRODUCT_COPY_FILES += \
+    device/google/taimen/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
+
+PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
@@ -22,4 +58,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := google/taimen/taimen:11/RP1A.201005.004.A1/6934943:user/release-keys
 
-$(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
+-include device/google/taimen/device-lineage.mk
